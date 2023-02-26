9000b0265f79e1e1663c6bab9941585e10b21806
ncextc <113550052+ncextc@users.noreply.github.com>
2023-02-26 10:30:16 +0530

    Add files via upload

---
+++ Day1/Question1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Question1{
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number :");
+		int num = sc.nextInt();
+		
+		String str = num % 2 == 0 ? "Even" : "Odd";
+		System.out.println(str);
+	}
+}
---
+++ Day1/Question7.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Question7{
+	public static void main(String[] args){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number : ");
+		int num = sc.nextInt();
+		String str = num >=0 ? "Positive":"Negative";
+		
+		System.out.println(str);
+	}
+}
